2dc06bf041220ea0d2e7f7b52665ee42055415b1
drakata27 <theall3xander@gmail.com>
2023-10-27 17:35:36 +0100

    Created Date Picker Application for exercise 2

--- Week3/Exercise2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Week3/Exercise2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submit = findViewById(R.id.submit);
+
+        MyDatePicker myDatePicker = new MyDatePicker();
+
+
+        submit.setOnClickListener(view -> {
+            myDatePicker.show(getSupportFragmentManager(),"My Date Picker");
+        });
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar mCalendar = Calendar.getInstance();
+        mCalendar.set(Calendar.YEAR, year);
+        mCalendar.set(Calendar.MONTH, month);
+        mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(mCalendar.getTime());
+        TextView displayDate = findViewById(R.id.displayDate);
+        displayDate.setText(selectedDate);
+
     }
 }
