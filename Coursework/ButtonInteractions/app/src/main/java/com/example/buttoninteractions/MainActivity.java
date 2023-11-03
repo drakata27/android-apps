c8e741feb7650a8d8fb89ad59313beaff6f339fe
Aleksandar Drakaliyski <theall3xander@gmail.com>
2023-11-03 15:06:27 +0000

    Finished button interactions

---
+++ Coursework/ButtonInteractions/app/src/main/java/com/example/buttoninteractions/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.buttoninteractions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.buttoninteractions.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding; // init binding
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // top left
+        binding.topLeftbtn.setOnClickListener(v -> {
+            showToast("You clicked the Top Left button!", 0, -500);
+        });
+
+        // top right
+        binding.topRightbtn.setOnClickListener(v -> {
+            showToast("You clicked the Top Right button!", 0, -500);
+        });
+
+        // center
+        binding.centerBtn.setOnClickListener(v -> {
+            showToast("You clicked the Center button!", 0, 0);
+        });
+
+        // bottom left
+        binding.bottmLeftBtn.setOnClickListener(v -> {
+            showToast("You clicked the Bottom Left button!", 0, 500);
+        });
+
+        // bottom right
+        binding.bottomRightBtn.setOnClickListener(v -> {
+            showToast("You clicked the Bottom Right button!", 0, 500);
+        });
+    }
+
+    private void showToast(String toastText, int xOffset, int yOffset) {
+            LayoutInflater inflater = getLayoutInflater();
+            View layout = inflater.inflate(R.layout.toast_custom, null);
+
+            TextView text = (TextView) layout.findViewById(R.id.toastMessage);
+            text.setText(toastText);
+
+            Toast toast = new Toast(getApplicationContext());
+            toast.setGravity(Gravity.CENTER_VERTICAL, xOffset, yOffset);
+            toast.setDuration(Toast.LENGTH_LONG);
+            toast.setView(layout);
+            toast.show();
+    }
+}
